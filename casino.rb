64349ed0b98ebd6ec3ac6e2dcbb4bb1329c47d66
Jonmagneson <47639626+Jonmagneson@users.noreply.github.com>
2019-02-15 14:28:45 -0700

    first menu

---
+++ casino.rb
@@ -0,0 +1,19 @@
+class Game
+  def initialize
+  menu
+  end
+
+  def menu
+    puts "Which game would you like to play?"
+    puts ["1) blackjack," "2) greenjack"]
+    game = gets.to_i
+    case game
+    when 1
+      blackjack
+    when 2
+      greenjack
+    else
+      menu
+    end
+  end
+end
