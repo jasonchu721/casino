2430beca83dcd4fa5275f77eee8f69650441d265
Jason Chu <jasonchu721@gmail.com>
2019-02-15 17:24:07 -0700

    minor tweak

--- menu.rb
+++ menu.rb
@@ -31,7 +31,7 @@ class Game
     when 3
       exit
     else
-      menu
+      game_menu
     end
   end
 
@@ -75,17 +75,12 @@ class Dice
   @die1 = []
   @die2 = []
   def initialize
-    puts "If dice total is 6 or higher you win!!".colorize(:blue)
-    space
     menu
-    show_dice
-    show_sum
-    win
   end
 
   def menu
+    puts "If dice total is 6 or higher you win!!".colorize(:blue)
     puts "Press 1 to roll".colorize(:yellow)
-
     case gets.to_i
       when 1
         roll
@@ -96,16 +91,19 @@ class Dice
   def roll
     @die1 = 1 + rand(6)
     @die2 = 1 + rand(6)
+    show_dice
   end 
 
   def show_dice
     space
-    print "Die-1: ", @die1, " Die-2:", @die2
+    print "Die-1: ", @die1, " Die-2: ", @die2
+    show_sum
   end
 
   def show_sum
     space
     print " Sum of dice is ", @die1 + @die2, ".\n"
+    win
   end
 
   def win
@@ -114,20 +112,33 @@ class Dice
       space
       sleep(1)
       puts "YOU WON".colorize(:green)
+      play_again
     else
       space
       sleep(1)
       puts "BETTER LUCK NEXT TIME".colorize(:red)
+      play_again
     end
+
   end
   def space 
     puts " " * 5
+  end
+  def play_again
+    puts "Do you want to play again? Yes or No?"
+    case gets.strip
+    when "yes"
+      menu
+    else
+      @game.game_menu
+    end
   end
 end
 
 
 
-game = Game.new
+
+@game = Game.new
 dice = Dice.new
 
 game
