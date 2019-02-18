64efd1cbd03a3ce7faaec94eb69ffc799efa9317
Sione Tenifa <47639594+shown17@users.noreply.github.com>
2019-02-17 17:32:15 -0700

    Update shown.rb

--- shown.rb
+++ shown.rb
@@ -1,69 +1,35 @@
-require "pry"
-require "colorize"
-class Dice
-
+def dice
   @die1 = []
   @die2 = []
-  def initialize
-    menu
-  end
+  puts "Welcome to the Dice Roll game #{@player}"
+  sleep(1)
+  puts "How much money would you like to bet"
+  bet=gets.to_i
+  sleep(1)
+  puts "If dice total is 6 or higher you win"
+  puts "Press 1 to roll."
 
-  def menu
-    puts "If dice total is 6 or higher you win!!".colorize(:blue)
-    puts "Press 1 to roll".colorize(:yellow)
-    case gets.to_i
-      when 1
-        roll
-      else
-        menu
-    end
-  end
-  def roll
-    @die1 = 1 + rand(6)
-    @die2 = 1 + rand(6)
-    show_dice
-  end 
-
-  def show_dice
-    space
-    print "Die-1: ", @die1, " Die-2: ", @die2
-    show_sum
-  end
-
-  def show_sum
-    space
-    print " Sum of dice is ", @die1 + @die2, ".\n"
-    win
-  end
-
-  def win
-    i = @die1 + @die2
-    if i >= 6
-      space
-      sleep(1)
-      puts "YOU WON".colorize(:green)
-      play_again
+  case gets.to_i
+    when 1
+      @die1 = 1 + rand(6)
+      @die2 = 1 + rand(6)
     else
-      space
-      sleep(1)
-      puts "BETTER LUCK NEXT TIME".colorize(:red)
-      play_again
-    end
-
+      dice
   end
-  def space 
-    puts " " * 5
+  print "Die-1: ", @die1, " Die-2: ", @die2
+  i = @die1 + @die2
+  if i >= 6
+    sleep(1)
+    puts "YOU WON".colorize(:green)
+  else
+    sleep(1)
+    puts "BETTER LUCK NEXT TIME".colorize(:red)
   end
-  def play_again
-    puts "Do you want to play again? Yes or No?"
+  puts "Do you want to play again? Yes or No?"
     case gets.strip
     when "yes"
-      menu
+      dice
     else
-      Game.game_menu
+      Game
     end
   end
-end
-
-
-Dice.new
